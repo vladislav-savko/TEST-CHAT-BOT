f1365fac710ed5dbabbfeb82cc938b5ca2240ba3
Vladislav <vlad.savko21@gmail.com>
2024-07-11 11:06:44 +0300

    gr, uk langs added

--- src/scripts/local/local.js
+++ src/scripts/local/local.js
@@ -1,12 +1,16 @@
 import en from "./en.js";
 import pl from "./pl.js";
 import ru from "./ru.js";
+import gr from "./gr.js";
+import uk from "./uk.js";
 
 const local = {
     en,
     ru,
     pl,
-    available: ["en, ru, pl"],
+    gr,
+    uk,
+    available: ["en, ru, pl, gr, uk"],
 };
 
 export default (lang = "en") => {
