807929ddb6166c7b0484e620ece7fdd64ff642cd
vladislav-savko <vlad.savko21@gmail.com>
2024-08-29 13:55:17 +0300

    Update telegramCallback.js

--- src/scripts/state/telegramCallback.js
+++ src/scripts/state/telegramCallback.js
@@ -2,6 +2,7 @@ import { getListingById } from "../utilits.js";
 
 export default async () => {
     $request.query = $request.query.replace("by ", '');
+    log($request.query);
     if (Number($request.query)) {
         const index = parseInt($request.query);
         await getListingById(index);
