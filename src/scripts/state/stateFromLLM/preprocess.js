ddd051580451e605851476417c8e7e8a90dfe973
Vladislav <vlad.savko21@gmail.com>
2024-11-20 21:36:41 +0300

    upd

--- src/scripts/state/stateFromLLM/preprocess.js
+++ src/scripts/state/stateFromLLM/preprocess.js
@@ -3,111 +3,112 @@ import axios from "axios";
 
 export default async () => {
     log("test");
-    // const language = async (text) => {
-    //     const response = await axios.post(
-    //         "https://caila.io/api/mlpgate/account/1000062767/model/51023/predict",
-    //         {
-    //             texts: [text],
-    //         },
-    //         {
-    //             headers: {
-    //                 "Content-Type": "application/json",
-    //                 "MLP-API-KEY":
-    //                     "1000160400.95801.PGCrF5ntqFXT6Nolbhg6Qfcbq5xeEJTjL1MMUEQi",
-    //             },
-    //         }
-    //     );
-    //     return response.data;
-    // };
+    const language = async (text) => {
+        const response = await axios.post(
+            "https://caila.io/api/mlpgate/account/1000062767/model/51023/predict",
+            {
+                texts: [text],
+            },
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                    "MLP-API-KEY":
+                        "1000160400.95801.PGCrF5ntqFXT6Nolbhg6Qfcbq5xeEJTjL1MMUEQi",
+                },
+            }
+        );
+        return response.data;
+    };
 
-    // const translate = async (text, sourceLang) => {
-    //     try {
-    //         const response = await axios.get(
-    //             `https://suapi.net/api/text/translate?to=en&text[]="${text}"`,
-    //             { timeout: 10000 }
-    //         );
+    const translate = async (text, sourceLang) => {
+        try {
+            const response = await axios.get(
+                `https://suapi.net/api/text/translate?to=en&text[]="${text}"`,
+                { timeout: 10000 }
+            );
 
-    //         if (response.status === 200) {
-    //             return response.data;
-    //         } else {
-    //             const fallbackResponse = await axios.get(
-    //                 `https://translate.cloudflare.jaxing.cc/?text=${encodeURIComponent(
-    //                     text
-    //                 )}&source_lang=${sourceLang}&target_lang=en`,
-    //                 { timeout: 10000 }
-    //             );
-    //             return fallbackResponse.data;
-    //         }
-    //     } catch (error) {
-    //         const fallbackResponse = await axios.get(
-    //             `https://translate.cloudflare.jaxing.cc/?text=${encodeURIComponent(
-    //                 text
-    //             )}&source_lang=${sourceLang}&target_lang=en`,
-    //             { timeout: 10000 }
-    //         );
-    //         return fallbackResponse.data;
-    //     }
-    // };
+            if (response.status === 200) {
+                return response.data;
+            } else {
+                const fallbackResponse = await axios.get(
+                    `https://translate.cloudflare.jaxing.cc/?text=${encodeURIComponent(
+                        text
+                    )}&source_lang=${sourceLang}&target_lang=en`,
+                    { timeout: 10000 }
+                );
+                return fallbackResponse.data;
+            }
+        } catch (error) {
+            const fallbackResponse = await axios.get(
+                `https://translate.cloudflare.jaxing.cc/?text=${encodeURIComponent(
+                    text
+                )}&source_lang=${sourceLang}&target_lang=en`,
+                { timeout: 10000 }
+            );
+            return fallbackResponse.data;
+        }
+    };
 
-    // const llm = async (input) => {
-    //     const response = await axios.post(
-    //         "http://213.149.180.145:58080/chat",
-    //         {
-    //             message: input,
-    //             code_prompt: "state-entity",
-    //         },
-    //         {
-    //             headers: {
-    //                 "Content-Type": "application/json",
-    //             },
-    //             timeout: 25000,
-    //         }
-    //     );
-    //     return response.data;
-    // };
+    const llm = async (input) => {
+        const response = await axios.post(
+            "http://213.149.180.145:58080/chat",
+            {
+                message: input,
+                code_prompt: "state-entity",
+            },
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                timeout: 25000,
+            }
+        );
+        return response.data;
+    };
 
-    // const getState = (state, data, input_text) => {
-    //     let nextState = state;
+    const getState = (state, data, input_text) => {
+        let nextState = state;
 
-    //     switch (state) {
-    //         case "Seller":
-    //             nextState = "/DisplayResult/ShowByPosition/Seller";
-    //             break;
-    //         case "Hello":
-    //             nextState = "/Start";
-    //             break;
-    //         case "ShowMore":
-    //             nextState = "/DisplayResult/ShowMore";
-    //             break;
-    //         default:
-    //             nextState = `/${state}`;
-    //             break;
-    //     }
+        switch (state) {
+            case "Seller":
+                nextState = "/DisplayResult/ShowByPosition/Seller";
+                break;
+            case "Hello":
+                nextState = "/Start";
+                break;
+            case "ShowMore":
+                nextState = "/DisplayResult/ShowMore";
+                break;
+            default:
+                nextState = `/${state}`;
+                break;
+        }
 
-    //     const phrases = [
-    //         {
-    //             regex: /^(hi( to)?|hello|greeting(s)?|hey|good (morning|afternoon|evening))$/i,
-    //             state: "/Start",
-    //         },
-    //         { regex: /^(info|information)$/i, state: "/InfoAbout" },
-    //         {
-    //             regex: /^(seller contacts|seller)$/i,
-    //             state: "/DisplayResult/ShowByPosition/Seller",
-    //         },
-    //     ];
+        const phrases = [
+            {
+                regex: /^(hi( to)?|hello|greeting(s)?|hey|good (morning|afternoon|evening))$/i,
+                state: "/Start",
+            },
+            { regex: /^(info|information)$/i, state: "/InfoAbout" },
+            {
+                regex: /^(seller contacts|seller)$/i,
+                state: "/DisplayResult/ShowByPosition/Seller",
+            },
+        ];
 
-    //     for (const phrase of phrases) {
-    //         if (phrase.regex.test(input_text.toLowerCase())) {
-    //             nextState = phrase.state;
-    //             break;
-    //         }
-    //     }
+        for (const phrase of phrases) {
+            if (phrase.regex.test(input_text.toLowerCase())) {
+                nextState = phrase.state;
+                break;
+            }
+        }
 
-    //     return nextState;
-    // };
+        return nextState;
+    };
 
-    // const text = $context.request.query;
-    // if (text[0] === "/") return true;
+    const text = $context.request.query;
+    
+    log(text);
 
     // language(text).then((lng_res) => {
     //     const lng = lng_res.scored_languages_list[0].languages[0];
