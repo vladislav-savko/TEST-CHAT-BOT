0044f729aff1b58acd0f326f2edbd92104ed9747
Vladislav <vlad.savko21@gmail.com>
2024-11-20 22:02:09 +0300

    upd

--- src/scripts/state/stateFromLLM/loader.js
+++ src/scripts/state/stateFromLLM/loader.js
@@ -2,5 +2,5 @@ import response from "../../response.js";
 
 export default async () => {
     response.channel("sendChatAction", { action: "typing" });
-    $reactions.transition("/Preprocess");
+    $reactions.transition("/Preprocess"); 
 };
