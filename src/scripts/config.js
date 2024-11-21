11266ef06f4dd691122d4321863999656e72848b
Vladislav <vlad.savko21@gmail.com>
2024-11-21 10:34:05 +0300

    upd

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,3 +1,4 @@
 export const API__LINK = "https://api.anisad.com";
 export const MAIN__LINK = "https://anisad.com";
 export const VERSION = '0.0.5';
+export const TG_TOKEN = "7488351900:AAFdeqMbzhYQD_4cxCSHr0ZyPTYom2GfAZ8";
--- src/scripts/state/stateFromLLM/loader.js
+++ src/scripts/state/stateFromLLM/loader.js
@@ -1,6 +1,21 @@
 import response from "../../response.js";
+import axios from "axios";
+import { TG_TOKEN } from "../../config.js";
 
 export default async () => {
-    response.channel("sendChatAction", { action: "typing" });
-    $reactions.transition("/Preprocess");
+    // response.channel("sendChatAction", { action: "typing" });
+    const chat_id = $context.rawRequest.message.chat.id;
+    await axios.post(
+        `https://api.telegram.org/bot${TG_TOKEN}/sendChatAction`,
+        {
+            chat_id,
+            action: "typing",
+        },
+        {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        }
+    );
+    await $reactions.transition("/Preprocess");
 };
