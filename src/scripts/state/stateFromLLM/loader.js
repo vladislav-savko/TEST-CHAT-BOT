f6b3e399bf0e04f39f78ded34b093a0550a85b86
Vladislav <vlad.savko21@gmail.com>
2024-11-21 14:28:20 +0300

    upd

--- src/scripts/state/stateFromLLM/loader.js
+++ src/scripts/state/stateFromLLM/loader.js
@@ -5,18 +5,18 @@ import { TG_TOKEN } from "../../config.js";
 export default async () => {
     // response.channel("sendChatAction", { action: "typing" });
     log($context);
-    // const chat_id = $context.rawRequest.message.from.id;
-    // await axios.post(
-    //     `https://api.telegram.org/bot${TG_TOKEN}/sendChatAction`,
-    //     {
-    //         chat_id,
-    //         action: "typing",
-    //     },
-    //     {
-    //         headers: {
-    //             "Content-Type": "application/json",
-    //         },
-    //     }
-    // );
+    const chat_id = $context.request.rawRequest.message.from.id;
+    await axios.post(
+        `https://api.telegram.org/bot${TG_TOKEN}/sendChatAction`,
+        {
+            chat_id,
+            action: "typing",
+        },
+        {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        }
+    );
     await $reactions.transition("/Preprocess");
 };
