7aa9c1d022862351fc595e4e2dbb255826d8b622
vladislav-savko <vlad.savko21@gmail.com>
2024-12-11 15:58:16 +0300

    upd

--- src/scripts/state/stateFromLLM/preprocess.js
+++ src/scripts/state/stateFromLLM/preprocess.js
@@ -162,5 +162,5 @@ export default async () => {
     const answerData = llm_.data;
 
     $session.lastData = JSON.parse(answerData || "{}");
-    // $reactions.transition(getState(answerState, t_text));
+    $reactions.transition(getState(answerState, t_text));
 };
