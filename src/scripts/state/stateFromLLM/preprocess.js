11216df16dfea4e83b077687a730516539eba726
Vladislav <vlad.savko21@gmail.com>
2024-12-23 10:38:09 +0300

    upd

--- src/scripts/state/stateFromLLM/preprocess.js
+++ src/scripts/state/stateFromLLM/preprocess.js
@@ -142,6 +142,12 @@ export default async () => {
             text
         );
     const hasKeywords = /(up|down|from|to)/i.test(text);
+    if ($context.session.contextPath === '/InputData/InputPrice') {
+        const words = ['Any', 'Unlimited'];
+        if (words.includes(text.toLowerCase())) {
+            text = `Budget ${text}`
+        }
+    }
 
     if (isNumberOnly && !hasKeywords) {
         t_text = `budget to ${text}`;
