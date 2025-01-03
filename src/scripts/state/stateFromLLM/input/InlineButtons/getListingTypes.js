e1a8f408ff67f8aad6423c68944322b66d152a31
vladislav-savko <vlad.savko21@gmail.com>
2025-01-03 11:01:20 +0300

    upd

--- src/scripts/state/stateFromLLM/input/InlineButtons/getListingTypes.js
+++ src/scripts/state/stateFromLLM/input/InlineButtons/getListingTypes.js
@@ -1,3 +1,6 @@
+import local from "../../../../local/local.js";
+import response from "../../../../response.js";
+
 export default async () => {
     const TYPES = {
         RENT: "RENT",
@@ -11,6 +14,17 @@ export default async () => {
     };
 
     const value = await $request.query.split('GET_LISTING_')[1];
+
+    const reply = {
+        body: {
+            text: `${$request.rawRequest.callback_query.message.text}\n\n✅ *${local(lang).getProperty.listingType[value]}*`,
+            parse_mode: "Markdown",
+            message_id: $request.rawRequest.callback_query.message.message_id,
+        },
+        method: "editMessageText",
+    };
+
+    response.channel([reply]);
 
     switch (value) {
         case "RENT":
--- src/scripts/state/stateFromLLM/input/InlineButtons/getPrice.js
+++ src/scripts/state/stateFromLLM/input/InlineButtons/getPrice.js
@@ -1,24 +1,53 @@
+import local from "../../../../local/local.js";
+import response from "../../../../response.js";
+
 export default async () => {
-    const applyType = async (value) => {
+    const applyType = async (value, text) => {
+        const reply = {
+            body: {
+                text: `${$request.rawRequest.callback_query.message.text}\n\n✅ *${text}*`,
+                parse_mode: "Markdown",
+                message_id:
+                    $request.rawRequest.callback_query.message.message_id,
+            },
+            method: "editMessageText",
+        };
+
+        response.channel([reply]);
+
         $session.lastData = value;
         $reactions.transition("/InputData");
     };
 
     const value = await $request.query.split("GET_PRICE_")[1];
 
+    let price = "";
+
     if (value !== "ANY") {
         const parsedRange = value.split("-").map(Number);
+        price = parsedRange[1]
+            ? `${local(lang).getProperty.price.from} ${parsedRange[0]} ${
+                  local(lang).getProperty.price.to
+              } ${parsedRange[1]}`
+            : `${local(lang).getProperty.price.from} ${parsedRange[0]}`;
 
         if (parsedRange?.length) {
-            applyType({
-                priceFrom: parsedRange[0],
-                priceTo: parsedRange[1],
-            });
+            applyType(
+                {
+                    priceFrom: parsedRange[0],
+                    priceTo: parsedRange[1],
+                },
+                price
+            );
         }
     } else {
-        applyType({
-            priceFrom: 0,
-            priceTo: null,
-        });
+        price = local(lang).getProperty.price[value];
+        applyType(
+            {
+                priceFrom: 0,
+                priceTo: null,
+            },
+            price
+        );
     }
 };
